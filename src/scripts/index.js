f09768f5b46597d21cd4535753f593aa7f60afd9
Keenan Irvin-Benning <kirvinb@outlook.com>
2019-03-30 18:26:13 -0400

    iife vs objects

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,36 @@
-if (5 === 5){
-    let message = 'Equal';
-}
-console.log(message);
+import { relative } from "path";
+
+let al = (function(){
+    let carId = 213;
+    let getId = function(){
+        return carId;
+    };
+    let updateId = function(newId){
+        carId = newId;
+        return carId;
+    };
+    return {
+        getId: getId,
+        updateId: updateId
+    };
+})();
+console.log(al.getId());
+console.log(al.updateId(123));
+console.log(al.getId());
+
+let susan = {
+    peopleId : 512,
+    getPeopleId: function(){
+        return this.peopleId;
+    },
+    updatePeopleId: function(newUd){
+        this.peopleId = newUd;
+        return this.peopleId;
+    }
+};
+
+console.log(susan.getPeopleId());
+
+console.log(susan.updatePeopleId(256));
+
+console.log(susan.getPeopleId());
