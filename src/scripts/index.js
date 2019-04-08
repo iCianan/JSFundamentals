9a287cbba8d3042e824f9243b0e60e4550e0b4cf
Keenan Irvin Benning <Keenan.IrvinBenning@twomen.com>
2019-04-08 15:52:02 -0400

    modifying dom elements

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,55 +1,5 @@
-import { relative } from "path";
-import {Planet} from './models/planet.js';
+let p = document.getElementById('para');
 
-let Car = {
-    carId: 123,
-    style: 'coupe',
-    transmission: 'manual'
-};
-
-function Plane(id, type){
-    this.planeId = id;
-    this.engineType = type;
-}
-
-String.prototype.hello = function() {
-    return this + " Hello sir";
-};
-
-Plane.prototype.start = function() {
-    console.log('Starting...' + this.engineType);
-};
-
-
-
-console.log(JSON.stringify(Car));
-
-console.log('lol'.hello());
-
-let hellcat = new Plane(123, 'V16');
-hellcat.start();
-
-let planet = new Planet();
-
-class Earth extends Planet{
-    rotate(){
-        return console.log('Rotating: for 24 hours');
-    }
-}
-
-let earth = new Earth(123);
-earth.rotate();
-console.log(earth.type);
-// class Jupiter extends Planet{
-//     rotate(){
-//         return console.log('Rotating: for 10 hours');
-//     }
-// }
-// let earth = new Earth();
-// earth.rotate();
-// console.log(earth.type);
-
-
-// let jupiter = new Jupiter();
-// jupiter.rotate();
-// console.log(jupiter.type);
+setTimeout(() => {
+    p.classList.add('p1');    
+}, 3000);
