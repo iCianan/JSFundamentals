bda84c1b38ef82ab19ece03f139ba2b4f387000c
Keenan Irvin-Benning <keenan@catalystdigitalventures.com>
2019-10-28 23:24:24 -0400

    dynamic prog

--- src/scripts/models/questions/nthfibonacci.js
+++ src/scripts/models/questions/nthfibonacci.js
@@ -7,3 +7,27 @@ export function getNthFib(number) {
 		return getNthFib(number - 1) + getNthFib(number - 2);
 	}
 }
+
+export function fibonacciMaster() {
+	let cache = {};
+	return function fib(n) {
+		if (n in cache) {
+			return cache[n];
+		} else {
+			if (n <= 2) {
+				return 1;
+			} else {
+				cache[n] = fib(n - 1) + fib(n - 2);
+				return cache[n];
+			}
+		}
+	};
+}
+
+export function fibPro(n, memo = []) {
+	if (memo[n !== undefined]) return memo[n];
+	if (n <= 2) return 1;
+	let results = fib(n - 1, memo) + fib(n - 2, memo);
+	memo[n] = results;
+	return results;
+}
